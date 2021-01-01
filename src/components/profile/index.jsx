3a85e81e1204c1a39a00708c32e1dab7f08551c1
Douglas Hammarstam <dogge20@hotmail.com>
2021-01-01 16:22:09 +0100

    get user by name profile

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Profile from './profile'
 import axios from 'axios'
 
-const url = 'http://localhost:8010/userByName'
+const url = 'http://localhost:8010/userByName/kamal'
 
 const ProfileIndex = () => {
     const [username, setUsername] = useState('')
@@ -12,17 +12,17 @@ const ProfileIndex = () => {
         axios.get(url).then(response => {
             setUsername(response.data.username)
             setCategories(response.data.categories)
+            console.log(response)
         })
     }
 
     useEffect(() => {
-        return () => {
-            fetchUserInformation()
-        }
+        fetchUserInformation()
+
     }, [])
 
     return (
-        < Profile fetchUserInformation={fetchUserInformation} username={username} categories={categories} />
+        < Profile username={username} categories={categories} />
     )
 }
 
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Profile = ({ fetchUserInformation, username, categories }) => {
+const Profile = ({ username, categories }) => {
 
     return (
         <>
