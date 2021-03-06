ebcefb5f2c2ad9f0cec027c23a0a0ed46abba0b8
Douglas <dogge20@hotmail.com>
2021-03-06 20:16:09 +0100

    hej

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -6,14 +6,14 @@ import Link from 'next/link'
 const Profile = ({ username }) => {
 
     const play = () => {
-
+        Router
     }
 
     return (
         <>
             <h1>Welcome {username}, please choose categories</h1>
             <Dropdown />
-            <Button>Play</Button>
+            <Button>Play hej</Button>
         </>
     )
 }
